63fae19dcd3e00a8641c7308ea6632c5149ed491
thamannaa <77442887+thamannaa@users.noreply.github.com>
2021-05-19 12:07:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+rover_width = 120;
+rover_height = 70;
+rover_image = "https://specials-images.forbesimg.com/imageserve/5f53fbab099efa47007b0912/960x0.jpg?fit=scale";
+rover_x = 10;
+rover_y = 10;
